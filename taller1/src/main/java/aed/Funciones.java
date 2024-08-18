9edf9aaabcc849eca6e412f79677065a614cd251
thelpix <ivan123456274@gmail.com>
2024-08-17 21:53:28 -0300

    mitad primer taller

--- taller1/src/main/java/aed/Funciones.java
+++ taller1/src/main/java/aed/Funciones.java
@@ -1,41 +1,57 @@
 package aed;
-
+import java.lang.Math;
 class Funciones {
     int cuadrado(int x) {
-        // COMPLETAR
-        return 0;
+        return x*x;
     }
 
     double distancia(double x, double y) {
-        // COMPLETAR
-        return 0.0;
+        return Math.sqrt((x*x)+(y*y));
     }
 
     boolean esPar(int n) {
-        // COMPLETAR
-        return false;
+        return divideA(2,n);
+    }
+
+    boolean divideA(int d, int n) {
+        return (n % d == 0);
     }
 
     boolean esBisiesto(int n) {
-        // COMPLETAR
-        return false;
+        return ((divideA(4,n) && !divideA(100, n)) || divideA(400, n));
     }
 
     int factorialIterativo(int n) {
-        // COMPLETAR
-        return 0;
+        int res = 1;
+        while (n != 0) {
+            res *= n;
+            n -= 1;
+        }
+        return res;
     }
 
     int factorialRecursivo(int n) {
-        // COMPLETAR
-        return 0;
+        int res = 1;
+        if (n != 0) {
+            return factorialRecursivo(n-1)*n;
+        }
+        return res;
     }
 
     boolean esPrimo(int n) {
-        // COMPLETAR
-        return false;
+        return primo(n);
     }
 
+    boolean primo(int n) {
+        int contador = 0;
+        for (int i=1; i <= n; i++) {
+            if (divideA(i,n)) {
+                contador += 1;
+            }
+        }
+        return contador == 2;
+    }
+    
     int sumatoria(int[] numeros) {
         // COMPLETAR
         return 0;
