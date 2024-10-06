6ddb5353a89a1a300a82a416706fe4d26ccb6a30
thelpix <ivan123456274@gmail.com>
2024-10-05 23:54:36 -0300

    taller 2 terminado

--- taller2/src/main/java/aed/Agenda.java
+++ taller2/src/main/java/aed/Agenda.java
@@ -1,29 +1,40 @@
 package aed;
 
 public class Agenda {
-    
+    private Fecha fechaActual;
+    private ArregloRedimensionableDeRecordatorios recordatorio = new ArregloRedimensionableDeRecordatorios();
 
     public Agenda(Fecha fechaActual) {
-        // Implementar
+        this.fechaActual = fechaActual;
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
-        // Implementar
+        this.recordatorio.agregarAtras(recordatorio);
+    }
+
+    public String imprimirRecordatorios(ArregloRedimensionableDeRecordatorios listaRecordatorios) {
+        String imprimir = "";
+        //imprimir los recordatorios que coincidan fecha dentro del array de recordatorios
+        for (int i=0; i < listaRecordatorios.longitud(); i++) { 
+            //chequear si la fecha de un recordatorio i del array coincide con nuestra fecha
+            if ((listaRecordatorios.obtener(i).fecha()).equals(fechaActual)) {
+                imprimir += listaRecordatorios.obtener(i).toString() + "\n";
+            }
+        }
+        return imprimir;
     }
 
     @Override
     public String toString() {
-        // Implementar
-        return "";
+        return fechaActual.toString() + "\n" + "=====" + "\n" + imprimirRecordatorios(recordatorio);
     }
 
     public void incrementarDia() {
-        // Implementar
+        fechaActual.incrementarDia();
     }
 
     public Fecha fechaActual() {
-        // Implementar
-        return null;
+        return this.fechaActual.clonar();
     }
 
 }
