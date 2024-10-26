e25b5a51c6872ac64620dccb61f3cf6b7679f65b
thelpix <ivan123456274@gmail.com>
2024-10-26 00:28:38 -0300

    pensare con la almohada la funcion eliminar

--- taller4/src/main/java/aed/ABB.java
+++ taller4/src/main/java/aed/ABB.java
@@ -5,15 +5,16 @@ import java.util.*;
 // Todos los tipos de datos "Comparables" tienen el método compareTo()
 // elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> implements Conjunto<T> {
-    // Agregar atributos privados del Conjunto
+    // Agregar atributos privados del Conjunto, similar a lista doblemente enlazada
     private Nodo _raiz;
+    private int cardinal;
 
     private class Nodo {
         // Agregar atributos privados del Nodo
-        private T valor;
-        private Nodo izq;
-        private Nodo der;
-        
+        T valor;
+        Nodo izq;
+        Nodo der;
+
         // Crear Constructor del nodo
         public Nodo(T valor) {
             this.valor = valor;
@@ -24,29 +25,128 @@ public class ABB<T extends Comparable<T>> implements Conjunto<T> {
 
     public ABB() {
         _raiz = null;
+        cardinal = 0;
     }
 
     public int cardinal() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return cardinal;
     }
 
     public T minimo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nodoActual = _raiz;
+        //si hay uno mas chico que el que tengo ahora
+        while(nodoActual.izq != null){
+            nodoActual = nodoActual.izq;
+        }
+        return nodoActual.valor;
     }
 
     public T maximo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nodoActual = _raiz;
+        //Si hay uno mas grande que el que tengo ahora
+        while(nodoActual.der != null){
+            nodoActual = nodoActual.izq;
+        }
+        return nodoActual.valor;
     }
 
     public void insertar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
+        Nodo nodoActual = _raiz;
+        Nodo nodoPadre = null;
+        //tengo que ver si pertenece el nodo
+        if(pertenece(elem)) {
+            return;
+        }
+        if(_raiz == null) {
+            _raiz = new Nodo(elem);
+            cardinal++;
+            return;
+        }
+        //no me gusta que haya copiado el while :(
+        while(nodoActual != null){
+            nodoPadre = nodoActual;
+            //cuando elem < nodoActual.valor
+            if(nodoActual.valor.compareTo(elem) > 0) {
+                nodoActual = nodoActual.izq;
+                
+            }
+            //cuando elem > nodoActual.valor
+            else {
+                nodoActual = nodoActual.der;
+            }
+        }
+        //el nuevo nodo ya tiene el valor
+        //debo hacer que el nuevo nodo este vinculado a su padre
+        nodoActual = new Nodo(elem);
+        if(elem.compareTo(nodoPadre.valor) > 0) {
+            nodoPadre.der = nodoActual;
+        }
+        else{
+            nodoPadre.izq = nodoActual;
+        }
+        cardinal++;
     }
 
     public boolean pertenece(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
+        //si no es t elem, seguir de forma recursiva
+        //si es null, entonces es que no esta
+        Nodo nodoActual = _raiz;
+        while(nodoActual != null)
+        {
+            //cuando elem < nodoActual.valor
+            if(nodoActual.valor.compareTo(elem) > 0) {
+                nodoActual = nodoActual.izq;
+                
+            }
+            //cuando elem > nodoActual.valor
+            else if(nodoActual.valor.compareTo(elem) < 0) {
+                nodoActual = nodoActual.der;
+            }
+            //cuando elem == nodoActual.valor
+            else {
+                return true;
+            }
+        }
+        return false;
+    }                       
 
     public void eliminar(T elem){
+        //cuando no esta
+        if(!pertenece(elem)){
+            return;
+        }
+        Nodo nodoActual = _raiz;
+        Nodo nodoHijoIzq = null; //no se si hace falta
+        Nodo nodoHijoDer = null; //no se si hace falta, veo
+        Nodo nodoPadre = null;
+        //buscar el nodo a borrar
+        while(nodoActual.valor != elem) {
+            nodoPadre = nodoActual
+            if(elem.compareTo(nodoActual.valor) > 0){
+                nodoActual = nodoActual.der;
+            }
+            else{
+                nodoActual = nodoActual.izq;
+            }
+        }
+        //desvincular al nodo padre si su nodo hijo no tiene hijos
+        if(nodoActual.izq == null && nodoActual.der == null){
+            nodoActual = null;
+        }
+        //flojera luego corrijo esto
+        if(nodoActual.izq != null && nodoActual.der == null){
+
+        }
+        if(elem.compareTo(nodoPadre.valor) > 0){
+            nodoPadre.der = null;
+        }
+        else{
+            nodoPadre.izq = null;
+        }
+            //cuando no tiene hijos
+            //cuando tiene un hijo
+            //cuando tiene 2 hijos
+        cardinal--;
         throw new UnsupportedOperationException("No implementada aun");
     }
 
